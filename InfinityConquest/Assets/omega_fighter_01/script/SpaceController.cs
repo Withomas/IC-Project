9899be7e0e5ba4ca43f3cb7c843d981163aa3524
Withomas <other.thomass@gmail.com>
2015-11-09 16:37:18 +0100

    First Scene commit

---
+++ InfinityConquest/Assets/omega_fighter_01/script/SpaceController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceController : MonoBehaviour {
+	private Rigidbody SpaceRigidBody;
+	// Use this for initialization
+	void Start () {
+		SpaceRigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float AxeHorizontal = Input.GetAxis ("Horizontal");
+		float AxeVertical = Input.GetAxis ("Vertical");
+
+
+
+		Debug.Log (AxeHorizontal);
+
+
+	}
+}
