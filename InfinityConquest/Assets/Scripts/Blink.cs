ec614c0810e9c87ac3dddad7fc25376349bf72b7
Withomas <other.thomass@gmail.com>
2015-11-13 21:12:37 +0100

    Ajouts de caméras

---
+++ InfinityConquest/Assets/Scripts/Blink.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blink : MonoBehaviour {
+
+	public Light BlinkingLight;
+	public float MaxIntensity = 4.0f;
+	public float MinIntensity = 0.0f;
+
+	private int factor;
+	private bool GoingUp;
+	// Use this for initialization
+	void Start () 
+	{
+		BlinkingLight.intensity = MinIntensity;
+		GoingUp = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GoingUp) {
+			BlinkingLight.intensity += 0.1f;
+		} else {
+			BlinkingLight.intensity -= 0.1f;
+		}
+
+		if ((BlinkingLight.intensity >= MaxIntensity) && GoingUp) {
+			GoingUp = false;
+		} 
+
+		if ((BlinkingLight.intensity <= MinIntensity) && !GoingUp) {
+			GoingUp = true;
+		}
+		Debug.Log (BlinkingLight.intensity.ToString());
+	}
+}
---
+++ InfinityConquest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	#region propriétés publiques
+	public GameObject Spaceship;
+	#endregion
+
+	#region propriétés privées
+	Vector3 offset;
+	Vector3 Rotation;
+	#endregion
+
+	void Start()
+	{
+		offset = transform.position - Spaceship.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = Spaceship.transform.position + offset;
+		Rotation = Spaceship.transform.eulerAngles;
+		transform.eulerAngles = Rotation;
+	}
+}
