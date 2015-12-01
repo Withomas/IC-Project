b08d7e7c941353ed6cdc4885323392a0981af228
ashais <sophie.cheour@gmail.com>
2015-12-01 16:20:38 +0100

    travail du mardi 1er decembre
    ajout du script pour accrocher le drapeau enemi au vaisseau allié en cas
    de collision

---
+++ InfinityConquest/Assets/AccrocheVaisseau.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccrocheVaisseau : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.name == "Drapeau_Ennemi") {
+			col.transform.parent =this.transform;
+		}
+	}
+}
