322dac7a95f4632bb2730db5d7b95da1c25a45e6
ashais <sophie.cheour@gmail.com>
2015-12-01 13:14:46 +0100

    travail du lundi 30 novembre
    on a fait un nouveau script de déploiement, intégrer un vaisseau allier
    et un vaisseau ennemi  ainsi que les flags

---
+++ InfinityConquest/Assets/couleurRouge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class couleurRouge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color =  new Color(0.2F, 0.3F, 1F, 1F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
