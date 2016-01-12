18cc2817633b1490440ae5dd7f8733a5d5ba85f2
ashais <sophie.cheour@gmail.com>
2016-01-12 15:51:04 +0100

    TTH - Ajout des menus , scores

---
+++ InfinityConquest/Assets/Scripts/main/loadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadOnClick : MonoBehaviour
+{
+
+	public void loadScene(string scene)
+    {
+		Application.LoadLevel(scene);
+    }
+}
